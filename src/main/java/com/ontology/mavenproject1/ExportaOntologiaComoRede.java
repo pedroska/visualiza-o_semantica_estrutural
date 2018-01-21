c9fa70fce533ffeab0d641b074a66371be0c2906
Pedro Ivo <Pedro Ivo@PedroNote.home>
2018-01-21 21:50:45 -0200

    Atualização do Código.

--- src/main/java/com/ontology/mavenproject1/ExportaOntologiaComoRede.java
+++ src/main/java/com/ontology/mavenproject1/ExportaOntologiaComoRede.java
@@ -26,7 +26,7 @@ import javax.ws.rs.PathParam;
  *
  * @author Pedro Ivo
  */
-@Path("/exportaOntologia/{type}")
+@Path("/exportaOntologia/{type}/{optional: .*}")
 public class ExportaOntologiaComoRede {
 
     //Variaveis
@@ -42,13 +42,16 @@ public class ExportaOntologiaComoRede {
     }
 
 /**
-* Retorna os dados da ontologia na forma de uma rede (estrutura ou indivíduos)
-* @param type : Tipo da rede formada (1-estrutural; 2-indivíduos)
+* Retorna os dados da ontologia na forma de uma rede (estrutura ou indivÃ­duos)
+* @param type : Tipo da rede formada (1-estrutural; 2-indivÃ­duos)
+* @param optional: marca o individuo especifico
+* @param equation: equacao que determina como sera medida a similaridade
 * @return texto do grafo que representa a ontologia
 */    
 @GET
 @Produces({MediaType.TEXT_PLAIN})
-public String ontologia(@PathParam("type") String type) {
+public String ontologia(@PathParam("type") String type, @PathParam("optional") String optional,
+        @PathParam("equation") String equation) {
     
     //Carregando arquivo .owl
     FileManager.get().addLocatorClassLoader(Main.class.getClassLoader());
@@ -60,12 +63,8 @@ public String ontologia(@PathParam("type") String type) {
     //java.nio.file.Path input = Paths.get(path, "vaquinhaPlusPlus.owl");
     java.nio.file.Path input = Paths.get(path, "vaquinha_importacao.owl");
     ontModel.read(input.toUri().toString(), "RDF/XML");
-//    Reasoner reasoner = ReasonerRegistry.getOWLReasoner();
-//    reasoner = reasoner.bindSchema(ontModel);
-//    OntModelSpec ontModelSpec = OntModelSpec.OWL_DL_MEM_TRANS_INF;
-//    ontModelSpec.setReasoner(reasoner);
 
-    //Type 1 para estrutura da ontologia e 2 para indivíduos
+    //Type 1 para estrutura da ontologia, 2 para individuos e 3 para classe especifica (similaridade)
     switch(type){
         case "1": 
             getClasses(ontModel);
@@ -75,6 +74,10 @@ public String ontologia(@PathParam("type") String type) {
         case "2":
             getIndividuals(ontModel);
             getRelations(ontModel);
+            break;
+        case "3":
+            getIndividuals(ontModel,optional);
+            //getSimilarity(ontModel, equation);
             break;
         default:
             this.node = ");";
@@ -88,7 +91,7 @@ public String ontologia(@PathParam("type") String type) {
 }
 
 /**
-* Busca todos as classes da ontologia e suas ligações de subclasses
+* Busca todos as classes da ontologia e suas ligaÃ§Ãµes de subclasses
 * @param model
 */
 public void getClasses(OntModel model) {
@@ -100,7 +103,7 @@ public void getClasses(OntModel model) {
     {
         OntClass thisClass = (OntClass) classes.next();
         
-        //Criando o nó caso não for uma classe lixo
+        //Criando o nÃ³ caso nÃ£o for uma classe lixo
         if(thisClass.getLocalName() != null){
             id++;
             classMaping.put(thisClass.getLocalName(), id);
@@ -126,7 +129,7 @@ public void getClasses(OntModel model) {
 }
 
 /**
-* Busca todos as classes da ontologia e suas ligações de subclasses
+* Busca todos as classes da ontologia e suas ligaÃ§Ãµes de subclasses
 * @param model
 */
 public void getIndividuals(OntModel model) {
@@ -138,7 +141,7 @@ public void getIndividuals(OntModel model) {
     {
         OntClass thisClass = (OntClass) classes.next();
         
-        //Criando o nó caso não for uma classe lixo
+        //Criando o nÃ³ caso nÃ£o for uma classe lixo
         if(thisClass.getLocalName() != null){
             
             ExtendedIterator individuals = thisClass.listInstances();
@@ -152,7 +155,7 @@ public void getIndividuals(OntModel model) {
                 id++;
                 classMaping.put(individual.getLocalName(), id);
                 
-                //No campo data posso fazer um laço para pegar todas data properties
+                //No campo data posso fazer um laÃ§o para pegar todas data properties
                 this.node += "{\n"+
                 "      id: '"+id+"',\n" +
                 "      label: '"+individual.getLocalName()+"',\n" +
@@ -175,7 +178,59 @@ public void getIndividuals(OntModel model) {
 }
 
 /**
-* Busca todos as ligações de subclasses da ontologia
+* Busca uma classe especÃ­fica da ontologia e suas ligaÃ§Ãµes de subclasses 
+* @param model
+* @param classe
+*/
+public void getIndividuals(OntModel model, String classe) {
+    
+    ExtendedIterator classes = model.listClasses();
+    int id = 0;
+    
+    while (classes.hasNext())
+    {
+        OntClass thisClass = (OntClass) classes.next();
+        
+        //Criando o nÃ³ caso nÃ£o for uma classe lixo e for igual ao que eu quero
+        if(thisClass.getLocalName() != null && thisClass.getLocalName().equals(classe)){
+            
+            ExtendedIterator individuals = thisClass.listInstances();
+            
+            Random randCol = new Random();
+            String color = String.format("#%06X", randCol.nextInt(0xFFFFFF+1)); 
+            
+            while(individuals.hasNext()){
+                Individual individual = (Individual)individuals.next();
+                
+                id++;
+                classMaping.put(individual.getLocalName(), id);
+                
+                //No campo data posso fazer um laÃ§o para pegar todas data properties
+                this.node += "{\n"+
+                "      id: '"+id+"',\n" +
+                "      label: '"+individual.getLocalName()+"',\n" +
+                "      x: Math.random(),\n" +
+                "      y: Math.random(),\n" +
+                "      size: 1,\n" +
+                "      color: '"+color+"',\n" +
+                "      data: {\n" +
+                "        name: '"+individual.getLocalName()+"',\n" +
+                "        value: '"+id+"', \n"+
+                "        value2: '"+(id+5)+"', \n"+                        
+                "      }\n" +
+                "},\n"    
+                ;
+            }
+        }
+    }
+    
+    if(this.node.endsWith(",\n")){
+        this.node = this.node.substring(0, this.node.length()-2) + ");\n";
+    }    
+}
+
+/**
+* Busca todos as ligaÃ§Ãµes de subclasses da ontologia
 * @param model
 */
 public void getSubclasses(OntModel model) {
@@ -187,7 +242,7 @@ public void getSubclasses(OntModel model) {
     {
         OntClass thisClass = (OntClass) classes.next();
         
-        //caso não for uma classe lixo
+        //caso nÃ£o for uma classe lixo
         if(thisClass.getLocalName() != null){
             
             ExtendedIterator subClasses = thisClass.listSubClasses();
@@ -200,7 +255,7 @@ public void getSubclasses(OntModel model) {
                     int idSub = this.classMaping.get(subclass.getLocalName());
                     idEdge++;
                     
-                    //Criando a ligação de subclasses
+                    //Criando a ligaÃ§Ã£o de subclasses
                     this.edge += "{\n" +
                     "      id: 'e"+idEdge+"',\n" +
                     "      label: 'subClassOf',\n" +
@@ -232,7 +287,7 @@ public void getObjectProperties(OntModel model) {
     {
         ObjectProperty thisProperty = (ObjectProperty) properties.next();
         
-        //caso não for uma propriedade lixo e que tenha range e domain
+        //caso nÃ£o for uma propriedade lixo e que tenha range e domain
         if(thisProperty.getLocalName() != null && thisProperty.listDomain() != null &&
                 thisProperty.listRange() != null){
             
@@ -240,7 +295,7 @@ public void getObjectProperties(OntModel model) {
             ArrayList<String> domain = new ArrayList<>();
             ArrayList<String> range = new ArrayList<>();
 // Usar model.listUnionClasses() ou algo assim para ver as property com mais de uma...
-            //Talvez eu precise colocar uma exceção pra quando o range ou domain for um valor (tipo date?)
+            //Talvez eu precise colocar uma exceÃ§Ã£o pra quando o range ou domain for um valor (tipo date?)
             ExtendedIterator domains = thisProperty.listDomain();
             while(domains.hasNext()){
                 OntClass dom = (OntClass) domains.next();
@@ -262,7 +317,7 @@ public void getObjectProperties(OntModel model) {
                     int idRange = this.classMaping.get( r );
                     
                     idEdge++;
-                    //Criando a ligação de subclasses
+                    //Criando a ligaÃ§Ã£o de subclasses
                     this.edge += "{\n" +
                     "      id: 'ed"+idEdge+"',\n" +
                     "      label: '"+thisProperty.getLocalName()+"',\n" +
@@ -319,7 +374,63 @@ public void getRelations(OntModel model) {
             int idDomain = this.classMaping.get( d );
             int idRange = this.classMaping.get( r );
 
-            //Criando a ligação de subclasses
+            //Criando a ligaÃ§Ã£o de subclasses
+            this.edge += "{\n" +
+            "      id: 'ed"+(idEdge++)+"',\n" +
+            "      label: '"+s.getPredicate().getLocalName()+"',\n" +
+            "      source: '"+idDomain+"',\n" +
+            "      target: '"+idRange+"',\n" +
+            "      color: '#000000',\n" +
+            "      hover_color: '#FC0',\n" +
+            "      type: 'arrow',\n" +
+            "      size: 1      \n" +
+            "    },\n"
+            ;
+        }
+    }
+
+    if(this.edge.endsWith(",\n"))
+        this.edge = this.edge.substring(0, this.edge.length()-2) + ");\n";
+}
+
+/**
+* Busca todas as propriedades de objeto da ontologia
+* @param model
+* @param equation
+*/
+public void getSimilarity(OntModel model, String equation) {
+    
+    int idEdge = 0;
+    
+    //Gambiarra:
+    ExtendedIterator properties = model.listObjectProperties();
+    int i = 0;
+    Map<String,Integer> objectMaping = new HashMap<>();
+    while(properties.hasNext()){
+        ObjectProperty obj = (ObjectProperty) properties.next();
+        objectMaping.put(obj.getLocalName(), i++);
+    }
+    
+    //Pegando todas as triplas da ontologia
+    StmtIterator stm = model.listStatements();
+    
+    while(stm.hasNext()){
+        Statement s = stm.next();
+        
+//      the subject is the resource from which the arc leaves
+//      the predicate is the property that labels the arc
+//      the object is the resource or literal pointed to by the arc
+        
+        //Se for uma object property eu adiciono a aresta
+        if(objectMaping.containsKey(s.getPredicate().getLocalName())){
+      
+            String d = s.getSubject().getLocalName();
+            String r = s.getObject().toString().substring(s.getObject().toString().lastIndexOf("#")+1);
+            
+            int idDomain = this.classMaping.get( d );
+            int idRange = this.classMaping.get( r );
+
+            //Criando a ligaÃ§Ã£o de subclasses
             this.edge += "{\n" +
             "      id: 'ed"+(idEdge++)+"',\n" +
             "      label: '"+s.getPredicate().getLocalName()+"',\n" +
